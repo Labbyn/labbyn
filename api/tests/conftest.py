ead9c224f9fdbd4bb75ddd793396ee5363cc826d
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2025-11-14 17:20:11 +0100

    Initialize test directory for backend

---
+++ api/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from fastapi.testclient import TestClient
+from app.main import app
+
+@pytest.fixture(scope="module")
+def test_client():
+    """
+    Pytest fixture to create a TestClient for the FastAPI app.
+    :return: TestClient instance
+    """
+    with TestClient(app) as client:
+        yield client
