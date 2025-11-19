7b2d45492becd879ce3eac2980a347c944a2e6ba
s26990-pj <115359523+AdrianKopczynski@users.noreply.github.com>
2025-11-19 11:18:12 +0100

    Added simple connection test

---
+++ api/tests/smoke-tests/test_smoke_connection.py
@@ -0,0 +1,12 @@
+import pytest
+import subprocess
+
+
+@pytest.mark.smoke
+def test_get_prometheus_instances():
+    cmd = "redis-cli ping"
+    try:
+        result = subprocess.run(cmd, shell=True, capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Command failed with return code: {e.returncode}. Error:{e.stderr}")
+    assert "PONG" in result.stdout
