8ddf1121b6a4a7fbac99cc0108052e32ff408423
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2025-12-10 23:24:39 +0100

    refresh redis connection for ci tests

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,11 +1,16 @@
 """Pytest configuration file for setting up test fixtures."""
-
+import asyncio
 import uuid
 from unittest import mock
+
 import pytest
+import redis.asyncio as redis_asyncio
+from fastapi.testclient import TestClient
+
 from app.database import SessionLocal
 from app.main import app
-from fastapi.testclient import TestClient
+from app.utils import redis_service
+from app.utils.redis_service import REDIS_URL
 
 
 @pytest.fixture(scope="module")
@@ -48,3 +53,33 @@ def db_session():
 def unique_category_name():
     """Genearate random category name to avoid unique problems"""
     return f"SmokeTest-GPU-{uuid.uuid4().hex[:8]}"
+
+
+@pytest.fixture(scope="function")
+def refresh_redis_client(monkeypatch):
+    """
+    Refresh redis client connection (for CI tests)
+    :param monkeypatch: Monkey patching fixture
+    :return: New redis client connection
+    """
+    loop = None
+    new_redis = redis_asyncio.from_url(REDIS_URL, encoding="utf-8", decode_responses=True)
+    monkeypatch.setattr(redis_service.redis_manager, "client", new_redis)
+    yield
+    try:
+        try:
+            loop = asyncio.get_event_loop()
+        except RuntimeError:
+            loop = None
+
+        if loop and loop.is_running():
+            loop.create_task(new_redis.aclose())
+        else:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(new_redis.aclose())
+            loop.close()
+    #pylint: disable=broad-exception-caught
+    except Exception:
+        # If we catch any exception that means redis loop is already deactived.
+        pass
