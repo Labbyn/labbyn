5ea4aa58b01eec94a4cea6f72b7e6c8228043137
s27081-pj <s27081@pjwstk.edu.pl>
2025-12-15 14:51:29 +0100

    Add docker compose dev

--- api/start-api.sh
+++ api/start-api.sh
@@ -20,4 +20,4 @@ echo "Applying database migrations..."
 alembic upgrade head
 
 echo "Starting Uvicorn server..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
