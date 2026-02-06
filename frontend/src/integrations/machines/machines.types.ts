539e032ea8080f5b37b881c14d14418b507fe732
Aleksander Stankowski <aleksander.stankowski@proton.me>
2026-02-06 21:14:51 +0100

    refactor: machines integration

--- frontend/src/integrations/machines/machines.types.ts
+++ frontend/src/integrations/machines/machines.types.ts
@@ -1,7 +1,7 @@
-export type ApiMachineItem = {
+export interface MachinesResponse {
   id: number
   name: string
-  localization_id: number | null
+  localization_id: number
   mac_address: string | null
   ip_address: string | null
   pdu_port: number | null
@@ -9,12 +9,62 @@ export type ApiMachineItem = {
   os: string | null
   serial_number: string | null
   note: string | null
-  added_on: string | null
   cpu: string | null
   ram: string | null
   disk: string | null
   metadata_id: number
   layout_id: number | null
-  version_id: number | null
+  added_on: string // format: date-time
+  version_id: number
 }
-export type ApiMachineResponse = Array<ApiMachineItem>
+
+export interface MetadataResponse {
+  id: number
+  last_update: string | null
+  agent_prometheus: boolean
+  ansible_access: boolean
+  ansible_root_access: boolean
+  version_id: number
+}
+
+export interface PlatformFormValues {
+  hostname: string
+  addToDb: boolean
+  scanPlatform: boolean
+  deployAgent: boolean
+  login?: string
+  password?: string
+  name?: string
+  ip?: string
+  mac?: string
+  location?: number
+  team?: number
+  pdu_port?: number
+  os?: string
+  sn?: string
+  note?: string
+  cpu?: string
+  ram?: string
+  disk?: string
+  layout?: number
+}
+
+export interface MachineUpdate {
+  name?: string | null
+  localization_id?: number | null
+  mac_address?: string | null
+  ip_address?: string | null
+  pdu_port?: number | null
+  team_id?: number | null
+  os?: string | null
+  serial_number?: string | null
+  note?: string | null
+  cpu?: string | null
+  ram?: string | null
+  disk?: string | null
+  layout_id?: number | null
+  metadata_id?: number | null
+}
+
+export type ApiMachineItem = MachinesResponse
+export type ApiMachineResponse = Array<MachinesResponse>
