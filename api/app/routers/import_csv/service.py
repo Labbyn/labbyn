e9dfb4828076fd5097deea538bdbc0196c6059f9
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2026-04-08 20:32:38 +0200

    raise non default exception to not lose row statuses

--- api/app/routers/import_csv/service.py
+++ api/app/routers/import_csv/service.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from fastapi import HTTPException, status
 from typing import Any, Dict, List
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -89,8 +90,9 @@ class ImportService:
 
         await self.db.commit()
         if len(data) > 0 and results["summary"]["success"] == 0:
-            raise exceptions.ValidationError(
-                message="Every import failed.",
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=results  # Przekazujemy całą strukturę z błędami wierszy
             )
 
         return results
