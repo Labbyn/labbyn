d560371f35e0f736ffec76f0e7dd09bd6147b5fd
s27081-pj <s27081@pjwstk.edu.pl>
2026-06-20 18:11:38 +0200

    Sidebar

--- frontend/src/integrations/category/category.mutation.ts
+++ frontend/src/integrations/category/category.mutation.ts
@@ -1,4 +1,6 @@
 import api from '@/lib/api'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import type { ApiCategoryItem } from './category.types'
 
 const PATHS = {
   BASE: '/db/categories',
@@ -10,4 +12,28 @@ export async function useCreateCategoryMutation(categoryData: {
 }) {
   const { data } = await api.post(PATHS.BASE, categoryData)
   return data
+}
+
+export const useDeletCategoryMutation = (categoryId: number) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationKey: ['delete-category'],
+    mutationFn: () => api.delete(PATHS.DETAIL(categoryId)),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['categories', 'list'] })
+    },
+  })
+}
+
+export const useUpdateCategoryMutation = (categoryId: number) => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationKey: ['update-category'],
+    mutationFn: (data: Partial<ApiCategoryItem> ) => api.patch(PATHS.DETAIL(categoryId), data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['categories', 'list'] })
+    },
+  })
 }
