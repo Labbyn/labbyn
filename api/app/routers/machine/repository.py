890a93f4756741a9f0ccb201d6f6de1df8fc31de
Patryk-Kosmider <s26985@pjwstk.edu.pl>
2026-06-29 20:20:23 +0200

    grafana: fix prod link

--- api/app/routers/machine/repository.py
+++ api/app/routers/machine/repository.py
@@ -9,7 +9,7 @@ from app.db import models
 GRAFANA_URL = (
     "http://localhost:3001"
     if os.environ.get("ENV") == "development"
-    else "http://grafana:3000"
+    else "http://localhost:3001/grafana/"
 )
 
 
